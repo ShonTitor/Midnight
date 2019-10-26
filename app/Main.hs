18909574511880aa6b1b67e7f38924ef0ea39145
ShonTitor <14-10576@usb.ve>
2019-10-25 20:28:26 -0400

    un mainsito al menos

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main (main) where
---import Parser (gato)
+import Parser (gato)
+import System.Environment
 
 main :: IO ()
 main = do
   putStrLn "Hola este es el main"
-  --s <- getLine
-  --a <- gato s
-  --putStrLn (show a)
+  (_:s:_) <- getArgs
+  _ <- gato s
   putStrLn "jaja salu2"
